730af326588f59bac8a81ef5a7f36d4eea669886
Boris Civcic <civcicboris93@gmail.com>
2018-08-18 16:32:59 +0200

    projects updated

--- src/reducers/reducer-projects8.js
+++ src/reducers/reducer-projects8.js
@@ -13,6 +13,20 @@ export default () => {
       skills: 'React.JS',
       url: 'https://projects.invisionapp.com/share/GT812M2SB#/screens/183220244',
       status: 'Team'
+    },
+    {
+      number: '3.',
+      title: 'Simple Blog(add/delete)',
+      skills: 'React.JS',
+      url: 'http://civcic.com/projects/blog',
+      status: 'Individual'
+    },
+    {
+      number: '4.',
+      title: 'Charts',
+      skills: 'React.JS',
+      url: 'http://civcic.com/projects/charts',
+      status: 'Individual'
     }
   ]
 }
