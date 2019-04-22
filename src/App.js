f14397a61440335ddb4973873e4f99e06de5ec00
Boris Civcic <civcicboris93@gmail.com>
2019-04-22 13:02:40 +0200

    header removed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Header from './components/header/header'
+// import Header from './components/header/header'
 import Projects from './components/projects/project'
 import './App.css';
 
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        {/* <Header /> */}
         <Projects />
       </div>
     );
